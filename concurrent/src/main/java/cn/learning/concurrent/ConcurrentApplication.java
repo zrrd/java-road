0991e2946f43a5ef3c6f83fc6d32b7ece2a2e476
hope <shaoyijiong@worken.cn>
2018-07-05 09:08:15 +0800

    将原来分散的项目放在了一起

--- concurrent/src/main/java/cn/learning/concurrent/ConcurrentApplication.java
+++ concurrent/src/main/java/cn/learning/concurrent/ConcurrentApplication.java
@@ -2,16 +2,14 @@ package cn.learning.concurrent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
-@EnableScheduling
 public class ConcurrentApplication {
 
 	public static void main(String[] args) {
-		ConcurrentApplication.run(ConcurrentApplication.class, args);
+		SpringApplication.run(ConcurrentApplication.class,args);
 	}
 }
