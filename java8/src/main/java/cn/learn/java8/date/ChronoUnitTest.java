86af7735e2b4b9cd4134f06b10fef2189ec104f3
邵益炯 <shaoyijiong@worken.cn>
2020-08-20 16:04:40 +0800

    计算两个日期的差值

--- java8/src/main/java/cn/learn/java8/date/ChronoUnitTest.java
+++ java8/src/main/java/cn/learn/java8/date/ChronoUnitTest.java
@@ -16,5 +16,8 @@ public class ChronoUnitTest {
     LocalDate nextWeek = localDate.plus(1, ChronoUnit.WEEKS);
     //直接加天
     LocalDate nextDay = localDate.plusDays(10);
+
+    // 计算两个日期的差值
+    ChronoUnit.DAYS.between(LocalDate.now(), LocalDate.now());
   }
 }
