63efa7634e22b43de5d3e2e2fe98ade654a3cfcc
meiliodas <shaoyijiong@gmail.com>
2022-11-21 20:36:38 +0800

    缓存行

--- jvm/src/main/java/cn/learn/jvm/jmm/CacheLineEffect.java
+++ jvm/src/main/java/cn/learn/jvm/jmm/CacheLineEffect.java
@@ -5,36 +5,87 @@ package cn.learn.jvm.jmm;
  * @date 2022/8/31
  */
 public class CacheLineEffect {
-    //考虑一般缓存行大小是64字节，一个 long 类型占8字节
-    static long[][] arr;
 
-    public static void main(String[] args) {
+    private static class T {
+        public volatile long x = 0L;
+    }
 
-        int size = 1024 * 1024;
+    /**
+     * long 8个字节位于同一个缓存行
+     */
+    public static T[] arr = new T[2];
 
-        arr = new long[size][];
-        for (int i = 0; i < size; i++) {
-            arr[i] = new long[8];
-            for (int j = 0; j < 8; j++) {
-                arr[i][j] = 0L;
+    static {
+        arr[0] = new T();
+        arr[1] = new T();
+    }
+
+    /**
+     * 位于同一个缓存行测试时间
+     */
+    private static void test1() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000_000L; i++) {
+                arr[0].x = i;
             }
-        }
-        long sum = 0L;
-        long marked = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < 8; j++) {
-                sum = arr[i][j];
-            }
-        }
-        System.out.println("[cache line]Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+        });
 
-        marked = System.currentTimeMillis();
-        for (int i = 0; i < 8; i += 1) {
-            for (int j = 0; j < size; j++) {
-                sum = arr[j][i];
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000_000L; i++) {
+                arr[1].x = i;
             }
-        }
-        System.out.println("[no cache line]Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("test1:" + (System.currentTimeMillis() - start));
+    }
+
+    private static class C {
+        public volatile long x = 0L;
+        // 填充7位让一个C就是占满一个缓存行
+        public volatile long x1, x2, x3, x4, x5, x6, x7;
+    }
+
+    public static C[] arr1 = new C[2];
+
+    static {
+        arr1[0] = new C();
+        arr1[1] = new C();
+    }
+
+
+    private static void test2() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000_000L; i++) {
+                arr1[0].x = i;
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000_000L; i++) {
+                arr1[1].x = i;
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("test2:" + (System.currentTimeMillis() - start));
+    }
+
+
+    /**
+     * 缓存行共享会稍微慢点
+     */
+    public static void main(String[] args) throws InterruptedException {
+        test1();
+        test2();
     }
 
 }
